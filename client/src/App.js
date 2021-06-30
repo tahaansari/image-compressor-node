e55ec5cfbbf2933e364e8723d18fb9ff522247c5
taha ansari <taha.ansari@schbang.in>
2021-06-30 13:58:28 +0530

    wip

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,7 @@ function App() {
   React.useEffect(() => {
     fetch("/api")
       .then((res) => res.json())
-      .then((data) => setData(data.message));
+      .then((data) => setData(data));
   }, []);
 
   return (
--- server/index.js
+++ server/index.js
@@ -1,3 +1,7 @@
+import imagemin from 'imagemin';
+import imageminJpegtran from 'imagemin-jpegtran';
+import imageminPngquant from 'imagemin-pngquant';
+
 // server/index.js
 const path = require('path');
 const express = require("express");
@@ -11,7 +15,21 @@ app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 // Handle GET requests to /api route
 app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
+    
+    // res.json({ message: "Hello from server!" });
+
+
+    const files = await imagemin(['https://images.unsplash.com/photo-1593642632823-8f785ba67e45?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1489&q=80'], {
+        plugins: [
+            imageminJpegtran(),
+            imageminPngquant({
+                quality: [0.6, 0.8]
+            })
+        ]
+    });
+
+    res.send(files);
+
 });
 
 // All other GET requests not handled before will return our React app
