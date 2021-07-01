66e2b02851ddec1008af21d1679e11090569c169
taha ansari <taha.ansari@schbang.in>
2021-07-01 18:19:34 +0530

    wip

--- server/index.js
+++ server/index.js
@@ -2,6 +2,12 @@
 import path from 'path';
 import express from "express";
 
+// import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
 const PORT = process.env.PORT || 3001;
 
 const app = express();
